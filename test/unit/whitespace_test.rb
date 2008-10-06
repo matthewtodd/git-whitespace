e8055bd6503840cee42c1a2d0fc778e75e429b86
Matthew Todd <matthew.todd@gmail.com>
2008-10-06 15:00:50 +0300

    Reintroduce whitespace.

--- test/unit/whitespace_test.rb
+++ test/unit/whitespace_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
+
 class WhitespaceTest < Test::Unit::TestCase
   context Finder do
     setup { @files = Finder.new.files(fixtures) }
+    
     should 'ignore vendor' do
       assert_does_not_contain @files, /vendor/
     end
+    
     should 'ignore .extension' do
       assert_does_not_contain @files, fixtures('lib', 'file_with_unusual.extension')
     end
+    
     should 'include fixtures.rb' do
       assert_contains @files, fixtures('lib', 'fixtures.rb')
     end
+    
     should 'include README' do
       assert_contains @files, fixtures('README')
     end
